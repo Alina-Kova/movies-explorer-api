fb056cb18062c55cfb23a9f66c5aa77786167dc8
Alina Vayer <lovering2007@mail.ru>
2021-10-24 17:30:27 +0300

    limiter&error 403 added

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const cors = require('cors');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { limiter } = require('./middlewares/limiter');
+const NotFoundError = require('./errors/not-found-err');
 require('dotenv').config();
 
-// Слушаем 3000 порт
-const { PORT = 3000 } = process.env;
-
 const app = express();
+
+const { PORT = 3000 } = process.env;
 
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
@@ -20,11 +21,11 @@ mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
 });
 
 const corsOptions = [
-  'http://localhost:3000',
-  'https://alina.mesto.nomoredomains.monster',
-  'https://api.alina.mesto.nomoredomains.monster',
-  'http://alina.mesto.nomoredomains.monster',
-  'http://api.alina.mesto.nomoredomains.monster',
+  // 'http://localhost:3000',
+  // 'https://alina.mesto.nomoredomains.monster',
+  // 'https://api.alina.mesto.nomoredomains.monster',
+  // 'http://alina.mesto.nomoredomains.monster',
+  // 'http://api.alina.mesto.nomoredomains.monster',
 ];
 
 // eslint-disable-next-line consistent-return
@@ -49,6 +50,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cors());
+app.use(limiter);
 
 app.use(requestLogger); // подключаем логгер запросов
 
@@ -60,7 +62,27 @@ app.get('/crash-test', () => {
 
 app.use(errorLogger);
 
+app.use('*', () => {
+  throw new NotFoundError('Запрашиваемый ресурс не найден.');
+});
+
+// обрабатываем ошибку 500
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+  next();
+});
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`App listening on port ${PORT}`);
 });
+
+require('dotenv').config();
