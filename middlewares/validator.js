5b6a5b5897559e7f729df836904cc77bf1148a01
Alina Vayer <lovering2007@mail.ru>
2021-10-24 22:36:26 +0300

    validation added

---
+++ middlewares/validator.js
@@ -0,0 +1,54 @@
+const { celebrate, Joi } = require('celebrate');
+const validator = require('validator');
+const IncorrectDataError = require('../errors/incorrect-data-err');
+
+const urlValidation = (url) => {
+  const value = validator.isURL(url, { protocols: ['http', 'https'], require_protocol: true });
+  if (!value) {
+    throw new IncorrectDataError('Неправильный формат cсылки.');
+  }
+};
+
+module.exports.validateSignup = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+});
+
+module.exports.validateLogin = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+});
+
+module.exports.validateUserData = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    name: Joi.string().required().min(2).max(30),
+  }),
+});
+
+module.exports.validateId = celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().hex().required().length(24),
+  }),
+});
+
+module.exports.validateNewMovie = celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().custom(urlValidation).required(),
+    trailer: Joi.string().custom(urlValidation).required(),
+    thumbnail: Joi.string().custom(urlValidation).required(),
+    movieId: Joi.number().required(),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+  }),
+});
